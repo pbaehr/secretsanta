3927650e6d2c86e855f3cf8ae2eaaaa4ff8f157b
Peter Baehr <baehr@abiresearch.com>
2012-11-27 16:59:27 -0500

    Remove leftover debug print

--- secretsanta/settings.py
+++ secretsanta/settings.py
@@ -73,8 +73,6 @@ STATICFILES_DIRS = (
     os.path.join(SITE_ROOT, 'static'),
 )
 
-print STATICFILES_DIRS
-
 # List of finder classes that know how to find static files in
 # various locations.
 STATICFILES_FINDERS = (
