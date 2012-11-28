b02d809970e4d5b9fc50599c8669682a5e076059
Peter Baehr <baehr@abiresearch.com>
2012-11-27 20:19:16 -0500

    Reset IP/timestamp as well as flag.

--- people/management/commands/reset_view_flags.py
+++ people/management/commands/reset_view_flags.py
@@ -7,4 +7,4 @@ class Command(BaseCommand):
     help = 'Reset viewed flag for all users (convenient when testing)'
 
     def handle(self, *args, **kwargs):
-        Person.objects.all().update(viewed=False)
+        Person.objects.all().update(viewed=False, viewed_at=None, ip='')
